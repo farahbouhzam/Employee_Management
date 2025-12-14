da2d414f9deddddc43c751be2230544c82d1eee8
Farah bouhzam <148548537+farahbouhzam@users.noreply.github.com>
2025-12-14 15:29:23 +0100

    Update AuthController.java

--- backend/src/main/java/com/example/employeemanagement/controller/AuthController.java
+++ backend/src/main/java/com/example/employeemanagement/controller/AuthController.java
@@ -19,7 +19,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.Parameter;
+
 
 import java.util.HashMap;
 import java.util.Map;
